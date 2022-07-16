73382463b3c766e941c8bbdb3beedb216b2f80cc
Melvin Welton <nivlem65@gmail.com>
2022-07-16 18:09:14 -0500

    variable

---
+++ assets/memphis.js
@@ -0,0 +1,11 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+window.alert(" Welcome to Robot Gladiators ");
+
+function fight(){
+    window.alert(" The FIGHT has begun!!!");
+}
+
+fight();
+
